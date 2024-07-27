866e3d0f6ecf90591916e7c6b403e2203032cbf5
Aditi Sharma <10aditi03@gmail.com>
2024-07-27 12:53:57 +0530

    finished

---
+++ quiz_website/src/assets/data.js
@@ -0,0 +1,43 @@
+export const data =[
+    {
+        question: "What country has the highest life expectancy?",
+        option1:"HongKong",
+        option2:"India",
+        option3:"Australia",
+        option4:"Pakistan",
+        ans:1,
+    },
+    {
+        question: "What is the capital city of Australia?",
+        option1:"Sydney",
+        option2:"Melbourne",
+        option3:"Canberra",
+        option4:"Brisbane",
+        ans:3,
+    },
+    {
+        question: "Who wrote the novel 1984?",
+        option1:"George Orwell",
+        option2:"J.K. Rowling",
+        option3:"F. Scott Fitzgerald",
+        option4:"Ernest Hemingway",
+        ans:1,
+    },
+    {
+        question: "In what year was the first iPhone released",
+        option1:"2005",
+        option2:"2007",
+        option3:"2008",
+        option4:"2010",
+        ans:2,
+    },
+    {
+        question: "What is the tallest mountain in the world?",
+        option1:"Mount Everest",
+        option2:"K2",
+        option3:"Mount Kilimanjaro",
+        option4:"Denali",
+        ans:1,
+    },
+
+]
